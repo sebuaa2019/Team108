d988e5eac2cc97ec1bcebc8eeeb1391f6d709607
Rorschach <bhggljy@163.com>
2019-06-10 20:12:29 +0800

    Update new Team108

---
+++ Team_108代码（终板）/src/scripts/script2.cpp
@@ -0,0 +1,60 @@
+#include "8_innovation_script.h"
+CInnovationScript::CInnovationScript()
+{
+    
+}
+
+CInnovationScript::~CInnovationScript()
+{
+
+}
+
+void CInnovationScript::Queue_goto_detect()
+{
+    stAct newAct;
+/*
+    newAct.nAct = ACT_GOTO;
+    newAct.strTarget = "1";
+    arAct.push_back(newAct);
+
+    newAct.nAct = ACT_MOVE;
+    newAct.fLinear_x = 0;
+    newAct.fLinear_y = 0;
+    newAct.fAngular_z = 0;
+    arAct.push_back(newAct);
+
+    newAct.nAct = ACT_GOTO;
+    newAct.strTarget = "master_location";
+    arAct.push_back(newAct);
+*/
+    
+    newAct.nAct = ACT_ADD_WAYPOINT;
+    newAct.strTarget = "user_set_2";
+    arAct.push_back(newAct);
+
+
+/*
+    newAct.nAct = ACT_GOTO;
+    newAct.strTarget = "master_location";
+    arAct.push_back(newAct);
+
+
+    newAct.nAct = ACT_SHOW_IMAGE;
+    newAct.strTarget = "~/image.jpg";
+    arAct.push_back(newAct);
+
+
+    newAct.nAct = ACT_GOTO;
+    newAct.strTarget = "1";
+    arAct.push_back(newAct);
+
+
+    newAct.nAct = ACT_SHOW_IMAGE;
+    newAct.strTarget = "~/image.jpg";
+    arAct.push_back(newAct);
+
+
+    newAct.nAct = ACT_GOTO;
+    newAct.strTarget = "master_location";
+    arAct.push_back(newAct);*/
+}
